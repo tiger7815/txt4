7b0327ae32688fce3b4c4eecd3240270fd9640d4
GulshanisHere <wallahcourse@gmail.com>
2024-05-12 23:45:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6693618294:AAGwLkv7gW7ECsKmSew8ywfgz_hU11xuvZU",
+             bot_token= "7028957545:AAFx3_K3l-MDObw-Ps6_Y5_TH0ylUgq5ShE",
              api_id= 22609670,
              api_hash= "3506d8474ad1f4f5e79b7c52a5c3e88d")
 
